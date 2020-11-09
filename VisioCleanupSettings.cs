9197897a24d1b6334576bad5a654108dd8579f6d
Jolyon Suthers <jolyon.suthers@gmail.com>
2020-11-10 09:55:29 +1000

    Initial setup code.

---
+++ VisioCleanupSettings.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="VisioCleanupSettings.cs" company="Jolyon Suthers">
+// Copyright (c) Jolyon Suthers. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace VisioCleanup
+{
+    /// <summary>
+    ///     Visio cleanup settings.
+    /// </summary>
+    public class VisioCleanupSettings
+    {
+    }
+}
