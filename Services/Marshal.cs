1c2e4af39a67890f468d9759f213f31da6bf2fb7
Jolyon Suthers <jolyon.suthers@gmail.com>
2020-11-11 21:53:18 +1000

    copy of GetActiveObject from netframework46

---
+++ Services/Marshal.cs
@@ -0,0 +1,68 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Marshal.cs" company="Jolyon Suthers">
+// Copyright (c) Jolyon Suthers. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+// ReSharper disable IdentifierTypo
+
+namespace VisioCleanup.Services
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Runtime.Versioning;
+    using System.Security;
+
+    /// <summary>
+    ///     Implementation of .Net Framework 4.6 System.Runtime.InteropServices.Marshal.GetActiveObject().
+    /// </summary>
+    internal class Marshal
+    {
+        /// <summary>
+        ///     Copy of System.Runtime.InteropServices.Marshal.GetActiveObject().
+        /// </summary>
+        /// <param name="progId">String program identifier.</param>
+        /// <returns>Object.</returns>
+        [SecurityCritical] // auto-generated_required
+        public static object GetActiveObject(string progId)
+        {
+            Guid classId;
+
+            // Call CLSIDFromProgIDEx first then fall back on CLSIDFromProgID if
+            // CLSIDFromProgIDEx doesn't exist.
+            try
+            {
+                CLSIDFromProgIDEx(progId, out classId);
+            }
+            catch (Exception)
+            {
+                CLSIDFromProgID(progId, out classId);
+            }
+
+            GetActiveObject(ref classId, IntPtr.Zero, out var obj);
+            return obj;
+        }
+
+        [DllImport("ole32.dll", PreserveSig = false)]
+        [ResourceExposure(ResourceScope.None)]
+        [SuppressUnmanagedCodeSecurity]
+        [SecurityCritical] // auto-generated
+        private static extern void CLSIDFromProgID([MarshalAs(UnmanagedType.LPWStr)] string progId, out Guid clsid);
+
+        [DllImport("ole32.dll", PreserveSig = false)]
+        [ResourceExposure(ResourceScope.None)]
+        [SuppressUnmanagedCodeSecurity]
+        [SecurityCritical] // auto-generated
+        private static extern void CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] string progId, out Guid clsid);
+
+        [DllImport("oleaut32.dll", PreserveSig = false)]
+        [ResourceExposure(ResourceScope.None)]
+        [SuppressUnmanagedCodeSecurity]
+        [SecurityCritical] // auto-generated
+        private static extern void GetActiveObject(
+            ref Guid rclsid,
+            IntPtr reserved,
+            [MarshalAs(UnmanagedType.Interface)] out object ppunk);
+    }
+}
