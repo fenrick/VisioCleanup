6847fcd44881866b891801e70d6abcfbbe59c3e8
Jolyon Suthers <jolyon.suthers@gmail.com>
2020-11-17 21:43:44 +1000

    Settings
    Some settings for read/writing to visio.

--- VisioCleanupSettings.cs
+++ VisioCleanupSettings.cs
@@ -12,5 +12,44 @@ namespace VisioCleanup
     /// </summary>
     public class VisioCleanupSettings
     {
+        /// <summary>
+        ///     Gets or sets Visio Height Field.
+        /// </summary>
+        public string? VisioHeightField { get; set; }
+
+        /// <summary>
+        ///     Gets or sets Visio LocPinX Field.
+        /// </summary>
+        public string? VisioLocPinXField { get; set; }
+
+        /// <summary>
+        ///     Gets or sets Visio LocPinY Field.
+        /// </summary>
+        public string? VisioLocPinYField { get; set; }
+
+        /// <summary>
+        ///     Gets or sets Visio PinX Field.
+        /// </summary>
+        public string? VisioPinXField { get; set; }
+
+        /// <summary>
+        ///     Gets or sets Visio PinY Field.
+        /// </summary>
+        public string? VisioPinYField { get; set; }
+
+        /// <summary>
+        ///     Gets or sets Visio Spacer.
+        /// </summary>
+        public double VisioSpacer { get; set; }
+
+        /// <summary>
+        ///     Gets or sets Visio Units.
+        /// </summary>
+        public string? VisioUnits { get; set; }
+
+        /// <summary>
+        ///     Gets or sets Visio Width field.
+        /// </summary>
+        public string? VisioWidthField { get; set; }
     }
 }
