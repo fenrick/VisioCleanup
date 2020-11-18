eeb1dba868d120edb4c2f815d23af4b9bfdbda32
fenrick <jolyon.suthers@gmail.com>
2020-11-18 20:00:16 +1000

    Shrink shape to child size.

--- Objects/DiagramShape.cs
+++ Objects/DiagramShape.cs
@@ -188,6 +188,45 @@ namespace VisioCleanup.Objects
         {
             return HashCode.Combine(this.VisioId);
         }
+        /// <summary>
+        ///     Shrink shape to size of internal shapes.
+        /// </summary>
+        /// <param name="leftPadding">Left padding.</param>
+        /// <param name="rightPadding">Right padding.</param>
+        /// <param name="bottomPadding">Bottom padding.</param>
+        /// <param name="topPadding">Top padding.</param>
+        public void ShrinkToChildren(double leftPadding, double rightPadding, double bottomPadding, double topPadding)
+        {
+            if (this.Children.Count <= 0)
+            {
+                return;
+            }
+
+            var newCorners = this.Corners;
+
+            // left side
+            var leftSide = this.Children.OrderBy(shape => shape.Corners.LeftSide)
+                .Select(shape => shape.Corners.LeftSide).Min();
+            newCorners.LeftSide = leftSide - leftPadding;
+
+            // right side
+            var rightSide = this.Children.OrderBy(shape => shape.Corners.RightSide)
+                .Select(shape => shape.Corners.RightSide).Max();
+            newCorners.RightSide = rightSide + rightPadding;
+
+            // bottom side
+            var bottomSide = this.Children.OrderBy(shape => shape.Corners.BottomSide)
+                .Select(shape => shape.Corners.BottomSide).Min();
+            newCorners.BottomSide = bottomSide - bottomPadding;
+
+            // top side
+            var topSide = this.Children.OrderBy(shape => shape.Corners.TopSide).Select(shape => shape.Corners.TopSide)
+                .Max();
+            newCorners.TopSide = topSide + topPadding;
+
+            this.Corners = newCorners;
+        }
+
         private void FindNeighbours()
         {
             // reset all shapes.
