b635625f7eb42bb79a32f21fbf90909c07789e81
fenrick <jolyon.suthers@gmail.com>
2020-12-03 21:12:18 +1000

    unused variables

--- Services/VisioCleanupService.cs
+++ Services/VisioCleanupService.cs
@@ -232,7 +232,7 @@ namespace VisioCleanup.Services
             var changed = false;
 
             // loop through children.
-            foreach (var _ in diagramShape.Children.Where(shape => this.AdjustDiagram(shape, maxRightSide)))
+            foreach (var _ in diagramShape.Children.Where(_ => this.AdjustDiagram(_, maxRightSide)))
             {
                 changed = true;
             }
