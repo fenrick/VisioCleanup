13fa38c146748765970a1e8c22892acdb5b0e151
fenrick <jolyon.suthers@gmail.com>
2021-02-22 15:25:38 +1000

    Introduce DiagramShape and set config into it.

---
+++ VisioCleanup.Core/Models/DiagramShape.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DiagramShape.cs" company="Jolyon Suthers">
+// Copyright (c) Jolyon Suthers. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace VisioCleanup.Core.Models
+{
+    using VisioCleanup.Core.Models.Config;
+
+    internal class DiagramShape
+    {
+        public static AppConfig AppConfig { get; set; }
+    }
+}
