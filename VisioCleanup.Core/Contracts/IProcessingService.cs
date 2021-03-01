571d86d0fb43289bf3ae52eecb3938e37b03cbbf
fenrick <jolyon.suthers@gmail.com>
2021-03-01 13:03:30 +1000

    processing services & display full dataset.

---
+++ VisioCleanup.Core/Contracts/IProcessingService.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IProcessingService.cs" company="Jolyon Suthers">
+// Copyright (c) Jolyon Suthers. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace VisioCleanup.Core.Contracts
+{
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+
+    using VisioCleanup.Core.Models;
+
+    /// <summary>
+    /// Implementers store and process diagram shapes for representing in visio.
+    /// </summary>
+    public interface IProcessingService
+    {
+        /// <summary>
+        /// Gets collection of all diagram shapes.
+        /// </summary>
+        Collection<DiagramShape> AllShapes { get; }
+
+        /// <summary>
+        /// Gets the digram shape at the top of the tree.
+        /// </summary>
+        DiagramShape? MasterShape { get; }
+
+        /// <summary>
+        /// Laysouts the diagramshapes based on appconfig.
+        /// </summary>
+        /// <returns>A task.</returns>
+        Task LayoutDataSet();
+    }
+}
