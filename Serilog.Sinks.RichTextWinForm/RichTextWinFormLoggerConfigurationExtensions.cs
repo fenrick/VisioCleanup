7431cfb8c6640e6755d1865e451c9a5d42b9cbda
fenrick <jolyon.suthers@gmail.com>
2021-05-07 15:03:48 +1000

    Update RichTextWinFormLoggerConfigurationExtensions.cs
    Clean up documentation.

--- Serilog.Sinks.RichTextWinForm/RichTextWinFormLoggerConfigurationExtensions.cs
+++ Serilog.Sinks.RichTextWinForm/RichTextWinFormLoggerConfigurationExtensions.cs
@@ -58,10 +58,7 @@ namespace Serilog
         /// A message template describing the format used to write to the sink. The default is
         /// "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}".
         /// </param>
-        /// <param name="restrictedToMinimumLevel">
-        /// The minimum level for events passed through the sink. Ignored when
-        /// <paramref name="levelSwitch" /> is specified.
-        /// </param>
+        /// <param name="restrictedToMinimumLevel">The minimum level for events passed through the sink.</param>
         /// <exception cref="System.ArgumentNullException">When <paramref name="loggerSinkConfiguration" /> is null.</exception>
         /// <exception cref="System.ArgumentNullException">When <paramref name="outputTemplate" /> is null.</exception>
         /// <returns>Configuration object allowing method chaining.</returns>
