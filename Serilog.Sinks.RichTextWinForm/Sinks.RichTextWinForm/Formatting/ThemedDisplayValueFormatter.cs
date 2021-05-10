a2e8f8c9c795114445f46a03efa41b146301727a
Jolyon Suthers <jolyon.suthers@gmail.com>
2021-05-10 20:42:50 +1000

    Update ThemedDisplayValueFormatter.cs
    duplicate to simplify logic. needs more work.

--- Serilog.Sinks.RichTextWinForm/Sinks.RichTextWinForm/Formatting/ThemedDisplayValueFormatter.cs
+++ Serilog.Sinks.RichTextWinForm/Sinks.RichTextWinForm/Formatting/ThemedDisplayValueFormatter.cs
@@ -60,8 +60,43 @@ namespace Serilog.Sinks.RichTextWinForm.Formatting
                         }
                     }
 
-                case ValueType when value is int || value is uint || value is long || value is ulong || value is decimal || value is byte || value is sbyte || value is short
-                                    || value is ushort || value is float || value is double:
+                case ValueType when value is int || value is uint || value is long:
+                    {
+                        using (this.ApplyStyle(output, RichTextThemeStyle.Number))
+                        {
+                            using StringWriter buffer = new();
+                            scalar.Render(buffer, format, this.formatProvider);
+                            output.AppendText(buffer.ToString());
+                        }
+
+                        break;
+                    }
+
+                case ValueType when  value is ulong || value is decimal || value is byte:
+                    {
+                        using (this.ApplyStyle(output, RichTextThemeStyle.Number))
+                        {
+                            using StringWriter buffer = new();
+                            scalar.Render(buffer, format, this.formatProvider);
+                            output.AppendText(buffer.ToString());
+                        }
+
+                        break;
+                    }
+
+                case ValueType when  value is sbyte || value is short || value is ushort:
+                    {
+                        using (this.ApplyStyle(output, RichTextThemeStyle.Number))
+                        {
+                            using StringWriter buffer = new();
+                            scalar.Render(buffer, format, this.formatProvider);
+                            output.AppendText(buffer.ToString());
+                        }
+
+                        break;
+                    }
+
+                case ValueType when  value is float || value is double:
                     {
                         using (this.ApplyStyle(output, RichTextThemeStyle.Number))
                         {
