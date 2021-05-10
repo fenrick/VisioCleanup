4303e2e293c4a9c7d965dce1b6ef113167de6cbd
Jolyon Suthers <jolyon.suthers@gmail.com>
2021-05-10 20:48:01 +1000

    Update ThemedDisplayValueFormatter.cs
    Simplify code.

--- Serilog.Sinks.WinForm/Sinks/WinForm/Formatting/ThemedDisplayValueFormatter.cs
+++ Serilog.Sinks.WinForm/Sinks/WinForm/Formatting/ThemedDisplayValueFormatter.cs
@@ -48,8 +48,34 @@ namespace Serilog.Sinks.WinForm.Formatting
                         break;
                     }
 
-                case ValueType when value is int || value is uint || value is long || value is ulong || value is decimal || value is byte || value is sbyte || value is short
-                                    || value is ushort || value is float || value is double:
+                case ValueType when value is int || value is uint || value is long:
+                    {
+                        using StringWriter buffer = new();
+                        scalar.Render(buffer, format, this.formatProvider);
+                        output.Write(buffer.ToString());
+
+                        break;
+                    }
+
+                case ValueType when value is ulong || value is decimal || value is byte:
+                    {
+                        using StringWriter buffer = new();
+                        scalar.Render(buffer, format, this.formatProvider);
+                        output.Write(buffer.ToString());
+
+                        break;
+                    }
+
+                case ValueType when value is sbyte || value is short || value is ushort:
+                    {
+                        using StringWriter buffer = new();
+                        scalar.Render(buffer, format, this.formatProvider);
+                        output.Write(buffer.ToString());
+
+                        break;
+                    }
+
+                case ValueType when value is float || value is double:
                     {
                         using StringWriter buffer = new();
                         scalar.Render(buffer, format, this.formatProvider);
