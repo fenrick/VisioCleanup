c7b89dd5d6393a4aae64e516e8675001d04f4b65
Jolyon Suthers <jolyon.suthers@gmail.com>
2021-09-06 21:31:44 +1000

    Code cleanup

--- Serilog.Sinks.WinForm/Sinks/WinForm/Formatting/ThemedValueFormatterState.cs
+++ Serilog.Sinks.WinForm/Sinks/WinForm/Formatting/ThemedValueFormatterState.cs
@@ -17,6 +17,6 @@ namespace Serilog.Sinks.WinForm.Formatting
 
         public bool IsTopLevel;
 
-        public ThemedValueFormatterState Nest() => new() { Output = this.Output };
+        public ThemedValueFormatterState Nest() => new ThemedValueFormatterState { Output = this.Output };
     }
 }
