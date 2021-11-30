bf1dfef33ec308d5a200f57bd1efcf1b54a0562f
Jolyon Suthers <jolyon.suthers@gmail.com>
2021-11-30 10:50:27 +1000

    vanilla template.

---
+++ VisioCleanup.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace VisioCleanup.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
