3544a135640ef63317d0571a34adefa1bba23efc
Jolyon Suthers <jolyon.suthers@gmail.com>
2022-01-14 13:11:21 +1000

    better async handling of buttons.

--- VisioCleanup.UI/Forms/MainForm.Designer.cs
+++ VisioCleanup.UI/Forms/MainForm.Designer.cs
@@ -106,7 +106,7 @@ public partial class MainForm
             processExcelDataSet.TabIndex = 2;
             processExcelDataSet.Text = "Excel Load";
             processExcelDataSet.UseVisualStyleBackColor = true;
-            processExcelDataSet.Click += new System.EventHandler(this.ProcessExcelDataSet_Click);
+            processExcelDataSet.Click += new System.EventHandler(async delegate(object? o, EventArgs args) { await this.ProcessExcelDataSet_ClickAsync(o, args); });
             // 
             // loadVisioObjects
             // 
@@ -118,7 +118,7 @@ public partial class MainForm
             loadVisioObjects.TabIndex = 3;
             loadVisioObjects.Text = "Visio Load";
             loadVisioObjects.UseVisualStyleBackColor = true;
-            loadVisioObjects.Click += new System.EventHandler(this.LoadVisioObjects_Click);
+            loadVisioObjects.Click += new System.EventHandler(async delegate(object? o, EventArgs args) { await this.LoadVisioObjects_ClickAsync(o, args); });
             // 
             // logSplitContainer
             // 
@@ -196,7 +196,7 @@ public partial class MainForm
             loadFromIServerButton.TabIndex = 1;
             loadFromIServerButton.Text = "iServer Load";
             loadFromIServerButton.UseVisualStyleBackColor = true;
-            loadFromIServerButton.Click += new System.EventHandler(this.LoadFromIServer_Click);
+            loadFromIServerButton.Click += new System.EventHandler(async delegate(object? o, EventArgs args) { await this.LoadFromIServer_ClickAsync(o, args); });
             // 
             // layoutDataSet
             // 
@@ -208,7 +208,7 @@ public partial class MainForm
             layoutDataSet.TabIndex = 4;
             layoutDataSet.Text = "Layout";
             layoutDataSet.UseVisualStyleBackColor = true;
-            layoutDataSet.Click += new System.EventHandler(this.LayoutDataSet_Click);
+            layoutDataSet.Click += new System.EventHandler(async delegate(object? o, EventArgs args) { await this.LayoutDataSet_ClickAsync(o, args); });
             // 
             // updateVisioDrawing
             // 
@@ -220,7 +220,7 @@ public partial class MainForm
             updateVisioDrawing.TabIndex = 5;
             updateVisioDrawing.Text = "Update Visio";
             updateVisioDrawing.UseVisualStyleBackColor = true;
-            updateVisioDrawing.Click += new System.EventHandler(this.UpdateVisioDrawing_Click);
+            updateVisioDrawing.Click += new System.EventHandler(async delegate(object? o, EventArgs args) { await this.UpdateVisioDrawing_ClickAsync(o, args); });
             // 
             // drawBitmapButton
             // 
--- VisioCleanup.UI/Forms/MainForm.cs
+++ VisioCleanup.UI/Forms/MainForm.cs
@@ -109,7 +109,7 @@ public partial class MainForm : Form
     /// <summary>The layout data set_ click.</summary>
     /// <param name="sender">The <paramref name="sender" /> .</param>
     /// <param name="eventArgs">The event args.</param>
-    private async void LayoutDataSet_Click(object sender, EventArgs eventArgs)
+    private async Task LayoutDataSet_ClickAsync(object sender, EventArgs eventArgs)
     {
         if (this.CheckProcessingService())
         {
@@ -165,7 +165,7 @@ public partial class MainForm : Form
     /// <summary>sThe load from iserver database.</summary>
     /// <param name="sender">The event sender.</param>
     /// <param name="e">The click event.</param>
-    private async void LoadFromIServer_Click(object sender, EventArgs e)
+    private async Task LoadFromIServer_ClickAsync(object sender, EventArgs e)
     {
         try
         {
@@ -196,7 +196,7 @@ public partial class MainForm : Form
     /// <summary>Load Visio Object Model.</summary>
     /// <param name="sender">The <paramref name="sender" /> .</param>
     /// <param name="eventArgs">The <paramref name="eventArgs" /> .</param>
-    private async void LoadVisioObjects_Click(object sender, EventArgs eventArgs)
+    private async Task LoadVisioObjects_ClickAsync(object sender, EventArgs eventArgs)
     {
         try
         {
@@ -226,7 +226,7 @@ public partial class MainForm : Form
     /// <summary>Activate the processing of Excel data set.</summary>
     /// <param name="sender">The <paramref name="sender" /> .</param>
     /// <param name="eventArgs">The <paramref name="eventArgs" /> .</param>
-    private async void ProcessExcelDataSet_Click(object sender, EventArgs eventArgs)
+    private async Task ProcessExcelDataSet_ClickAsync(object sender, EventArgs eventArgs)
     {
         try
         {
@@ -272,7 +272,7 @@ public partial class MainForm : Form
     /// <summary>sThe update visio drawing_ click.</summary>
     /// <param name="sender">The event sender.</param>
     /// <param name="e">The DPI change details.</param>
-    private async void UpdateVisioDrawing_Click(object sender, EventArgs e)
+    private async Task UpdateVisioDrawing_ClickAsync(object sender, EventArgs e)
     {
         if (this.CheckProcessingService())
         {
