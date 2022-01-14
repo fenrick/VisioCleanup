f83f9782d9de5fb96234dce8f9af42b822dabfed
Jolyon Suthers <jolyon.suthers@gmail.com>
2022-01-14 12:12:02 +1000

    replace with standard comment block

--- VisioCleanup.UI/Forms/MainForm.Designer.cs
+++ VisioCleanup.UI/Forms/MainForm.Designer.cs
@@ -30,7 +30,10 @@ public partial class MainForm
 
     #region Windows Form Designer generated code
 
-    /// <summary>Required method for Designer support - do not modify the contents of this method with the code editor.</summary>
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
     private void InitializeComponent()
     {
             this.components = new System.ComponentModel.Container();
